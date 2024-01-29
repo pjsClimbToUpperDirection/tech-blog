94d6e7c33d6d3fc7f73783e45f61e7fe5e0169fd
junsung park <pjs@bagjunseong-ui-Macmini.local>
2024-01-29 14:50:29 +0900

    24-01-29-2

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,32 +14,41 @@ export default function RootLayout({
     }: {
     children: React.ReactNode
 }) {
-    const ButtonClass : string = "ddr";
     // 현재는 목 데이터를 만들어 놓았으나 api를 통하여 실제 데이터를 가져올 시 형식에 맞게끔 수정 필요
     const navLinks = [
         { id: 1, list: "1-1", href: ""},
-        { id: 2, list: "3-4", href: ""}
+        { id: 2, list: "3-4", href: ""},
+        { id: 3, list: "3-2", href: ""}
     ]
+
+    const NavStyle = {
+        Root: {
+            fontSize: "30px",
+            fontWeight: "bold",
+        },
+        Buttons: "inline-block leading-none"
+
+    }
+
     return (
         <html lang="en">
         <body>
-        <div>
-            <nav id={"rootNav"}>
-                <div className={"flex h-14 justify-center"}>
-                    <h1 className={"bg-blue-400 max-w-40 "}>
-                        <Link href="/">
+        <div className={"grid justify-items-center"}>
+            <div className={"p-[5px]"}>
+                <div className={"w-full grid grid-cols-5"}>
+                    <div className={"text-black col-span-1 grid justify-items-center"}>
+                        <Link href="/" style={NavStyle.Root}>
                             Tech blog
                         </Link>
-                    </h1>
-                    <ul id={"rootNav-list"}>
-                        <li>
-                            {navLinks.map((each) => (
-                                <LayoutButton key={each.id} customClass={ButtonClass} href={each.href}>{each.list}</LayoutButton>
-                            ))}
-                        </li>
-                    </ul>
+                    </div>
+                    <div className={"col-span-3 flex p-[3px], pl-[10px]"}>
+                        {navLinks.map((each) => (
+                            <LayoutButton key={each.id} customClass={NavStyle.Buttons}
+                                          href={each.href}>{each.list}</LayoutButton>
+                        ))}
+                    </div>
                 </div>
-            </nav>
+            </div>
         </div>
         {children}
         <div>
